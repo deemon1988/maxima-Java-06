2847b02cb9d8ece7f6ccc7587ed27b5b6bcf1235
deemon1988 <dmn72835@gmail.com>
2022-12-29 20:22:01 +0300

    5.1 Шаблонизация с FreeMarker

--- src/main/java/org/example/DynamicPage.java
+++ src/main/java/org/example/DynamicPage.java
@@ -9,35 +9,25 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) throws Exception {
-
-        //StringBuilder собрать строку в цикле
-          //  склеить много строк - Stringformat
-        FileWriter writer = new FileWriter("result.html", false);
+public class DynamicPage {
+    public static void createPage(String filename) throws Exception {
+        FileWriter catWriter = new FileWriter(filename, false);
 
         String resourcesPath = App.class.getClassLoader().getResources("templates").nextElement().getPath();
-
         Configuration config = new Configuration(Configuration.VERSION_2_3_31);
         config.setDirectoryForTemplateLoading(new File(resourcesPath));
         config.setDefaultEncoding("UTF-8");
-        Map root = new HashMap<>();
-        root.put("title", "Данные кота");
 
+        Map root = new HashMap<>();
+        root.put("title", "Кот");
         Cat cat = new Cat("Мурзик",5,true);
         root.put("cat", cat);
 
-        Template template = config.getTemplate("index.html");
-        template.process(root, writer);
+        Template catTemplate = config.getTemplate("catTemplate.html");
+        catTemplate.process(root, catWriter);
 
-        writer.flush();
-        writer.close();
+        catWriter.flush();
+        catWriter.close();
 
-       DynamicPage.createPage("catResult.html");
     }
 }
